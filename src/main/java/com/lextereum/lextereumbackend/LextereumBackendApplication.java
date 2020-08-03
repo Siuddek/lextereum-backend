0425ab50dc902d7605853b65d1fc372070d2272c
Siudek <maciej.siudek@bettersolutions.pl>
2020-08-03 19:37:34 +0200

    add default spring project with configuration

---
+++ src/main/java/com/lextereum/lextereumbackend/LextereumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lextereum.lextereumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LextereumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LextereumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lextereum/lextereumbackend/LextereumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lextereum.lextereumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LextereumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
